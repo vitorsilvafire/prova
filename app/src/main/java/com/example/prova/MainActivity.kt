0c99b9db2aecc5b0b874d9b02b7b7556fd493b58
vitor silva <vitorsilvadeoliveira050@gmail.com>
2023-04-05 22:36:09 -0300

    prova

---
+++ app/src/main/java/com/example/prova/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.prova
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var txtMes: EditText
+    private lateinit var txtDia: EditText
+
+
+    private lateinit var imgSigno: ImageView
+    private lateinit var txtSigno: TextView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imgSigno = findViewById(R.id.imgSigno)
+        txtSigno = findViewById(R.id.txtSigno)
+
+        val signo = calcularSigno("10/02/1990")
+        findViewById<Button>(R.id.btnCalcular).setOnClickListener {
+            val signo = calcularSigno("${txtDia.text}/${txtMes.text}")
+            exibirSigno(signo)
+        }
+
+    }
+    private fun calcularSigno(DiadoAniversario: String) {
+        val dia = DiadoAniversario.substring(0, 2).toInt()
+        val mes = DiadoAniversario.substring(3, 5).toInt()
+
+        val imageView = when {
+            (dia >= 21 && mes == 3) || (dia <= 20 && mes == 4) -> R.drawable.aries
+            (dia >= 21 && mes == 4) || (dia <= 20 && mes == 5) -> R.drawable.touro
+            (dia >= 21 && mes == 5) || (dia <= 20 && mes == 6) -> R.drawable.gemeos
+            (dia >= 21 && mes == 6) || (dia <= 21 && mes == 7) -> R.drawable.cancer
+            (dia >= 22 && mes == 7) || (dia <= 22 && mes == 8) -> R.drawable.leao
+            (dia >= 23 && mes == 8) || (dia <= 22 && mes == 9) -> R.drawable.virgem
+            (dia >= 23 && mes == 9) || (dia <= 22 && mes == 10) -> R.drawable.libra
+            (dia >= 23 && mes == 10) || (dia <= 21 && mes == 11) -> R.drawable.escorpiao
+            (dia >= 22 && mes == 11) || (dia <= 21 && mes == 12) -> R.drawable.sargitario
+            (dia >= 22 && mes == 12) || (dia <= 20 && mes == 1) -> R.drawable.capricornio
+            (dia >= 21 && mes == 1) || (dia <= 19 && mes == 2) -> R.drawable.aquario
+            else -> R.drawable.peixes
+        }
+
+    }
+
+    private fun exibirSigno(signo: Unit) {
+        val idImagem = resources.getIdentifier(signo.toString(), "drawable", packageName)
+        imgSigno.setImageResource(idImagem)
+        txtSigno.text = "Seu signo é $signo"
+    }
+
+}
+
+
